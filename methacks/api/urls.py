f8d769f415197ca4abea03c29264d2848cfe9f4f
antarpreet11 <singhantarpreet11@gmail.com>
2023-05-07 08:33:02 -0400

    fix: custom model returns a value

--- methacks/api/urls.py
+++ methacks/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import main, signup_view, login_view
+from .views import main, signup_view, login_view, getClassification
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
+    path('classify/', getClassification, name='classify'),
     path('', main)
 ]
